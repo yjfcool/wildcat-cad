3bacf5c2a1268ac49f7fff516734e8539136403c
ghemingway <ghemingway@0c1abd82-8e4b-0410-87c0-cbc6d3ccef69>
2008-03-25 19:03:59 +0000

    Inclusion of MFC MDI interface.  Beginning of bringing up Win32 support details.

---
+++ Source/Application/Win32/child_view.cpp
@@ -0,0 +1,91 @@
+/*******************************************************************************
+* Copyright (c) 2007, 2008, CerroKai Development
+* All rights reserved.
+*
+* Redistribution and use in source and binary forms, with or without
+* modification, are permitted provided that the following conditions are met:
+*     * Redistributions of source code must retain the above copyright
+*       notice, this list of conditions and the following disclaimer.
+*     * Redistributions in binary form must reproduce the above copyright
+*       notice, this list of conditions and the following disclaimer in the
+*       documentation and/or other materials provided with the distribution.
+*     * Neither the name of CerroKai Development nor the
+*       names of its contributors may be used to endorse or promote products
+*       derived from this software without specific prior written permission.
+*
+* THIS SOFTWARE IS PROVIDED BY CerroKai Development ``AS IS'' AND ANY
+* EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
+* WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+* DISCLAIMED. IN NO EVENT SHALL CerroKai Development BE LIABLE FOR ANY
+* DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES
+* (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
+* LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND
+* ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+* (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
+* SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+********************************************************************************/
+
+#include "Application/Win32/stdafx.h"
+#include "Application/Win32/wildcat_app.h"
+#include "Application/Win32/child_view.h"
+
+#include <iostream>
+
+/***********************************************~***************************************************/
+
+
+CChildView::CChildView() {
+//	SetTimer();
+}
+
+
+CChildView::~CChildView() {
+}
+
+
+BEGIN_MESSAGE_MAP(CChildView, CWnd)
+	ON_WM_PAINT()
+//	ON_WM_SETFOCUS()
+//	ON_WM_KILLFOCUS()
+//	ON_WM_KEYUP()
+//	ON_WM_TIMER()
+	ON_WM_MOUSEMOVE()
+//	ON_WM_MOUSEWHEEL()
+//	ON_WM_LBUTTONDOWN()
+//	ON_WM_LBUTTONUP()
+//	ON_WM_RBUTTONDOWN()
+//	ON_WM_RBUTTONUP()
+//	ON_WM_SIZING()
+END_MESSAGE_MAP()
+
+
+
+BOOL CChildView::PreCreateWindow(CREATESTRUCT& cs) {
+	if (!CWnd::PreCreateWindow(cs))
+		return FALSE;
+
+	cs.dwExStyle |= WS_EX_CLIENTEDGE;
+	cs.style &= ~WS_BORDER;
+	cs.lpszClass = AfxRegisterWndClass(CS_HREDRAW|CS_VREDRAW|CS_DBLCLKS, 
+		::LoadCursor(NULL, IDC_ARROW), reinterpret_cast<HBRUSH>(COLOR_WINDOW+1), NULL);
+
+	return TRUE;
+}
+
+
+void CChildView::OnPaint()  {
+	CPaintDC dc(this); // device context for painting
+	
+	// TODO: Add your message handler code here
+	
+	// Do not call CWnd::OnPaint() for painting messages
+}
+
+
+void CChildView::OnMouseMove(UINT nFlags, CPoint point) {
+	std::cout << "Here\n";
+}
+
+
+/***********************************************~***************************************************/
+
