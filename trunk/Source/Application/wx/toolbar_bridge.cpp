7ee5a93b3caafdfeb3e4a96a01c8559dc4af50f6
graham.hemingway <graham.hemingway@0c1abd82-8e4b-0410-87c0-cbc6d3ccef69>
2008-09-17 12:26:40 +0000

    Maybe I should try to include new files the FIRST time I check in....could be a good idea.

---
+++ trunk/Source/Application/wx/toolbar_bridge.cpp
@@ -0,0 +1,163 @@
+/*******************************************************************************
+* Copyright (c) 2007, 2008, CerroKai Development
+* All rights reserved.
+*
+* Redistribution and use in source and binary forms, with or without
+* modification, are permitted provided that the following conditions are met:
+*     * Redistributions of source code must retain the above copyright
+*       notice, this list of conditions and the following disclaimer.
+*     * Redistributions in binary form must reproduce the above copyright
+*       notice, this list of conditions and the following disclaimer in the
+*       documentation and/or other materials provided with the distribution.
+*     * Neither the name of CerroKai Development nor the
+*       names of its contributors may be used to endorse or promote products
+*       derived from this software without specific prior written permission.
+*
+* THIS SOFTWARE IS PROVIDED BY CerroKai Development ``AS IS'' AND ANY
+* EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
+* WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+* DISCLAIMED. IN NO EVENT SHALL CerroKai Development BE LIABLE FOR ANY
+* DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES
+* (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
+* LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND
+* ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+* (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
+* SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+********************************************************************************/
+
+
+/*** Included Header Files ***/
+#include "Application/wx/toolbar_bridge.h"
+#include "Application/wx/wildcat_app.h"
+#include "Application/wx/main_frame.h"
+#include "Utility/log_manager.h"
+#include "Application/toolbar_manager.h"
+#include "Kernel/document.h"
+#include "Kernel/workbench.h"
+
+
+/***********************************************~***************************************************/
+
+
+WCToolbarButton_Bridge::WCToolbarButton_Bridge(const WCUserMessage &message, const std::string &tooltip,
+	const unsigned int &type, const std::string &stdIcon, const std::string &activeIcon, const bool &active,
+	const bool &enabled) : _button(NULL), _message(message), _tooltip(tooltip), _type(type), _stdIcon(stdIcon),
+	_activeIcon(activeIcon), _isActive(active), _isEnabled(enabled) {
+	//Nothing else for now
+}
+
+
+
+WCToolbarButton_Bridge::~WCToolbarButton_Bridge() {
+	//Delete the wx button base
+	//...
+}
+
+
+//void WCToolbarButton_Bridge::AddInstance(WCToolbarButton_Win32* instance) {
+//	//Do nothing
+//}
+
+
+void WCToolbarButton_Bridge::IsActive(const bool &state) {
+	//Set the active flag (if needed)
+	if (this->_isActive != state) {
+		this->_isActive = state;
+	}
+}
+
+
+void WCToolbarButton_Bridge::IsEnabled(const bool &state) {
+	//Set the enabled flag (if needed)
+	if (this->_isEnabled != state) {
+		this->_isEnabled = state;
+	}
+}
+
+
+/***********************************************~***************************************************/
+
+
+WCToolbar_Bridge::WCToolbar_Bridge(WCMainFrame *frame, WCToolbar* toolbar, const std::string &title, const WCVector4 &position) : 
+	_frame(frame),	_isVisible(false), _title(title), _core(toolbar), _platform(NULL), _nextToolID(FIRST_TOOLBAR_BUTTON_ID) {
+	//Create platform toolbar
+	this->_platform = this->_frame->AddToolBar(wxConvertMB2WX(title.c_str()));
+	//Make sure is not nil
+	if (this->_platform == NULL) {
+		CLOGGER_ERROR(WCLogManager::RootLogger(), "WCToolbar_Bridge::WCToolbar_Bridge - Not able to create platform toolbar");
+	}
+	//Add the toolbar to wx
+	this->_platform->Realize();
+	this->_frame->AuiManager()->AddPane(this->_platform, wxAuiPaneInfo().Name(wxConvertMB2WX(title.c_str())).Caption(wxConvertMB2WX(title.c_str())).ToolbarPane().Top().Left());
+	this->_frame->AuiManager()->GetPane(this->_platform).Show();
+}
+
+
+WCToolbar_Bridge::~WCToolbar_Bridge() {
+	//Release platform
+	if (this->_platform != NULL) {
+		//Close the toolbar
+		delete this->_platform;
+	}
+}
+
+
+void WCToolbar_Bridge::OnButtonPress(WCToolbarButton_Bridge *button) {
+	std::cout << "What, I'm here?\n";
+	//Get bridge button
+//	WCToolbarButton_Bridge* bridge = button->Bridge();
+//	//If button is enabled, pass along
+//	if (bridge->IsEnabled()) {
+//		//Get user message and pass to active workbench
+//		this->_document->ActiveWorkbench()->OnUserMessage( bridge->Message() );
+//	}
+}
+
+
+void WCToolbar_Bridge::OnCloseButtonPress(void) {
+	//Pass to the platform
+	this->_platform->Show(false);
+	//Update the flag
+	this->_isVisible = false;
+}
+
+
+void WCToolbar_Bridge::AddButton(WCToolbarButton_Bridge *button) {
+	//Create wx Button object with appropriate parameters
+	wxString msg = wxConvertMB2WX(button->Message().c_str());
+	wxString tooltip = wxConvertMB2WX(button->Tooltip().c_str());
+	//m_type = type;
+	wxString stdIcon = wxConvertMB2WX(button->StandardIcon().c_str());
+	wxString activeIcon = wxConvertMB2WX(button->ActiveIcon().c_str());
+	//Call to platform to add the toolbar button
+	wxString imagePath = wxGetApp().GetExeFolder() + _T("/Resources/") + stdIcon + _T(".tiff");
+	wxToolBarToolBase* tool = this->_frame->AddToolBarTool(this->_platform, this->_nextToolID, msg, wxBitmap(imagePath, wxBITMAP_TYPE_TIF), tooltip);
+	//Update the toolbar
+	this->_platform->Realize();
+	this->_nextToolID++;
+}
+
+
+WCToolbarButton* WCToolbar_Bridge::ButtonFromName(const std::string &name) {
+	std::cout << "What am I doing here?\n";
+	//Get the toolbar manager from the document
+//	WCToolbarManager *manager = this->_document->ToolbarManager();
+	//Request button from manager
+//	return manager->ButtonFromName(name);
+	return NULL;
+}
+
+
+void WCToolbar_Bridge::IsVisible(const bool &state) {
+	//Work with visCount
+	if (state != this->_isVisible) {
+		//Pass to the platform
+		this->_platform->Show(state);
+		//Update the flag
+		this->_isVisible = state;
+	}
+}
+
+
+/***********************************************~***************************************************/
+
---
+++ trunk/Source/Application/wx/toolbar_bridge.h
@@ -0,0 +1,116 @@
+/*******************************************************************************
+* Copyright (c) 2007, 2008, CerroKai Development
+* All rights reserved.
+*
+* Redistribution and use in source and binary forms, with or without
+* modification, are permitted provided that the following conditions are met:
+*     * Redistributions of source code must retain the above copyright
+*       notice, this list of conditions and the following disclaimer.
+*     * Redistributions in binary form must reproduce the above copyright
+*       notice, this list of conditions and the following disclaimer in the
+*       documentation and/or other materials provided with the distribution.
+*     * Neither the name of CerroKai Development nor the
+*       names of its contributors may be used to endorse or promote products
+*       derived from this software without specific prior written permission.
+*
+* THIS SOFTWARE IS PROVIDED BY CerroKai Development ``AS IS'' AND ANY
+* EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
+* WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+* DISCLAIMED. IN NO EVENT SHALL CerroKai Development BE LIABLE FOR ANY
+* DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES
+* (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
+* LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND
+* ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+* (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
+* SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+********************************************************************************/
+
+
+#ifndef __TOOLBAR_BRIDGE_H__
+#define __TOOLBAR_BRIDGE_H__
+
+
+/*** Included Header Files ***/
+#include "Utility/wutil.h"
+#include "Utility/vector.h"
+
+
+/*** Namespace Declaration ***/
+namespace __WILDCAT_NAMESPACE__ {
+
+
+/*** C++ Class Predefines ***/
+class WCToolbarButton;
+class WCToolbar;
+class WCMainFrame;
+class WCToolbar_Bridge;
+
+
+/***********************************************~***************************************************/
+
+
+class WCToolbarButton_Bridge {
+public:
+	wxToolBarToolBase							*_button;											//!< Underlying wx button object
+	std::string									_message;											//!< Message
+	std::string									_tooltip;											//!< Tooltip for the button
+	int											_type;												//!< Type of button
+	std::string									_stdIcon, _activeIcon;								//!< Icon pointers
+	bool										_isActive;											//!< Is button active
+	bool										_isEnabled;											//!< Is button enabled
+public:
+	//Constructors and Destructors
+	WCToolbarButton_Bridge(const WCUserMessage &message, const std::string &tooltip,				//!< Primary constructor
+												const unsigned int &type, const std::string &stdIcon, 
+												const std::string &activeIcon, const bool &active, const bool &enabled);
+	~WCToolbarButton_Bridge();																		//!< Default destructor
+
+	//Member Access Methods
+	inline WCUserMessage Message(void) const	{ return this->_message; }							//!< Get the user message
+	inline std::string Tooltip(void)			{ return this->_tooltip; }							//!< Get the tooltip
+	inline std::string StandardIcon(void)		{ return this->_stdIcon; }							//!< Get the standard icon
+	inline std::string ActiveIcon(void)			{ return this->_activeIcon; }						//!< Get the active icon
+	inline int Type(void) const					{ return this->_type; }								//!< Get the button type
+	inline bool IsActive(void) const			{ return this->_isActive; }							//!< Get the active state
+	void IsActive(const bool &state);																//!< Set the active state
+	inline bool IsEnabled(void) const			{ return this->_isEnabled; }						//!< Get the enabled state
+	void IsEnabled(const bool &state);																//!< Set the enabled state
+};
+
+
+/***********************************************~***************************************************/
+
+
+class WCToolbar_Bridge {
+private:
+	WCMainFrame									*_frame;											//!< Associated main frame
+	bool										_isVisible;											//!< Visibility flag
+	std::string									_title;												//!< Title of the toolbar
+	WCToolbar									*_core;												//!< Core toolbar pointer
+	wxToolBar									*_platform;											//!< Platform toolbar pointer
+	int											_nextToolID;										//!< Counter for toolbar button placement
+public:
+	//Constructors and Destructors
+	WCToolbar_Bridge(WCMainFrame *frame, WCToolbar* toolbar, const std::string &title,				//!< Primary constructor
+												 const WCVector4 &position);
+	~WCToolbar_Bridge();																			//!< Default destructor
+
+	//Event Methods
+	void OnButtonPress(WCToolbarButton_Bridge *button);												//!< Triggered when an element is pressed
+	void OnCloseButtonPress(void);																	//!< Called when close button is pressed
+
+	//Member Access Methods
+	void AddButton(WCToolbarButton_Bridge *button);													//!< Add a button to the toolbar
+	WCToolbarButton* ButtonFromName(const std::string &name);										//!< Get a button from a name
+	inline bool IsVisible(void) const			{ return this->_isVisible; }						//!< Get the visible state
+	void IsVisible(const bool &state);																//!< Set the visible state
+};
+
+
+
+/***********************************************~***************************************************/
+
+
+}	   //End Wildcat Namespace
+#endif //__TOOLBAR_BRIDGE_H__
+
