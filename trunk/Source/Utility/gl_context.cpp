84f84970f6897873b0bd3a5e52c62cd7c0b162e4
graham.hemingway <graham.hemingway@0c1abd82-8e4b-0410-87c0-cbc6d3ccef69>
2008-12-15 20:33:05 +0000

    Starting to move towards global shared context and single initialization.

---
+++ trunk/Source/Utility/gl_context.cpp
@@ -0,0 +1,11 @@
+/*
+ *  gl_context.cpp
+ *  Wildcat
+ *
+ *  Created by Graham Hemingway on 12/15/08.
+ *  Copyright 2008 __MyCompanyName__. All rights reserved.
+ *
+ */
+
+#include "gl_context.h"
+
